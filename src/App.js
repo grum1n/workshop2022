a539a15e9b62f1fc718b9aec563ddc6f14cb9f57
grum1n <grumin.el.pastas@gmail.com>
2022-01-19 19:50:11 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
+
+export const calculateAge = (todayDate, enteredDate) => {
+  const ageDate =  todayDate - enteredDate;
+  return Math.floor(ageDate / 31536000000)
+}
+
+
 function App() {
+
+  const [enterData, setEnterData] = useState(null);
+  const [age, setAge] = useState("");
+  let today = new Date();
+
+
+  const submitHandler = () => { 
+      console.log("today: ", today.getTime(), " entered: ", enterData.getTime())
+    setAge(calculateAge(today.getTime(),enterData.getTime()));
+
+  }
+ 
+  const changeDateHandler = (e) => {
+    setEnterData(new Date(e.target.value))
+  
+  } 
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', flexDirection: 'column' , width: '300px'}}>
+        <label>Date:</label>
+        <input type='date' id='start' name='trip-start' onChange={changeDateHandler}/>
+        <label>Age:</label>
+        <h1>{age}</h1>
+        <button onClick={submitHandler}>calculate</button>
+
+        </div>
+        test
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import {calculateAge } from './App';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('test  calculateAge', () => {
+  expect(calculateAge(1642613895371, 1296604800000)).toEqual(10)
 });
+
+
+// test('renders learn submitHandlerk', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
